d4fc7420a56c3ce48a8623d61330ea28a1d5dec4
XiaoWuTu <LZ.XWT@outlook.com>
2023-05-14 09:19:12 +0800

    5.14

--- main.py
+++ main.py
@@ -1,107 +1,79 @@
-import sys
+import random
+import tkinter as tk
 import os
-import json
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QMessageBox, QDesktopWidget, QWidget
-
-class ClassroomLottery(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("课堂抽学号")
-        self.setFixedSize(300, 200)
-        self.move(QDesktopWidget().availableGeometry().center() - self.frameGeometry().center())
-
-        self.teacher_mode = True
-
-        self.label = QLabel(self)
-        self.label.setText("点击按钮抽取学号")
-        self.label.move(80, 40)
-
-        self.button = QPushButton("抽取", self)
-        self.button.move(110, 70)
-        self.button.clicked.connect(self.lottery)
-
-        self.score_count = {f"{i:03}" for i in range(1, 56)}
-        self.score = {key: 0 for key in self.score_count}
-
-        self.view_score_button = QPushButton("查看分数", self)
-        self.view_score_button.move(30, 160)
-        self.view_score_button.clicked.connect(self.view_score)
-
-        self.group_button = QPushButton("设置分组", self)
-        self.group_button.move(170, 160)
-        self.group_button.clicked.connect(self.set_group)
-
-        self.group = {}
-        self.load_group()
-
-    def load_group(self):
-        if os.path.exists('group.json'):
-            with open('group.json', 'r') as f:
-                self.group = json.load(f)
-
-    def save_group(self):
-        with open('group.json', 'w') as f:
-            json.dump(self.group, f)
-
-    def lottery(self):
-        student_numbers = [str(i).zfill(3) for i in range(1, 56)]
-        lucky_number = str(student_numbers[0])
-        if lucky_number not in self.group:
-            self.group[lucky_number] = "未分组"
-        dialog = QMessageBox()
-        dialog.setWindowTitle("是否获得加分？")
-        dialog.setText(f"学号{lucky_number}是否获得1分？")
-        dialog.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
-        result = dialog.exec()
-        if result == QMessageBox.Yes:
-            self.score[lucky_number] += 1
-        else:
-            dialog = QMessageBox()
-            dialog.setWindowTitle("是否扣分？")
-            dialog.setText(f"学号{lucky_number}是否扣1分？")
-            dialog.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
-            result = dialog.exec()
-            if result == QMessageBox.Yes:
-                self.score[lucky_number] -= 1
-        self.label.setText(f"恭喜你，获得学号 {lucky_number}")
-        dialog = QMessageBox()
-        dialog.setWindowTitle("抽到的学号")
-        dialog.setText(f"您抽到的学号是：{lucky_number}，分组为：{self.group[lucky_number]}")
-        dialog.exec()
-
-    def view_score(self):
-        score_text = ""
-        for key, value in self.score.items():
-            score_text += f"学号{key}：{value}分\n"
-        dialog = QMessageBox()
-        dialog.setWindowTitle("同学分数")
-        dialog.setText(score_text)
-        dialog.exec()
-
-    def set_group(self):
-        dialog = QMessageBox()
-        dialog.setWindowTitle("设置分组")
-        dialog.setText("请输入学号和分组名，以逗号隔开：")
-        dialog.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
-        result = dialog.exec()
-        if result == QMessageBox.Ok:
-            text = dialog.textValue().strip()
-            if "," not in text:
-                QMessageBox.warning(self, "错误", "输入格式错误，请输入学号和分组名，以逗号隔开。")
-                return
-            key, value = text.split(",")
-            key = key.strip()
-            value = value.strip()
-            if key not in self.score:
-                QMessageBox.warning(self, "错误", "输入学号不存在，请重新输入。")
-                return
-            self.group[key] = value
-            self.save_group()
-            QMessageBox.information(self, "设置成功", f"学号{key}的分组已设为{value}。")
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = ClassroomLottery()
+import sys
 
 
+def draw_student_number(min_number, max_number):
+    return random.randint(min_number, max_number)
 
+
+def update_label():
+    min_number = int(min_entry.get())
+    max_number = int(max_entry.get())
+    drawn_number = draw_student_number(min_number, max_number)
+    label.config(text=f"被抽中的学号是: {drawn_number}")
+
+
+def save_range():
+    min_number = min_entry.get()
+    max_number = max_entry.get()
+    with open('range.txt', 'w') as f:
+        f.write(f"{min_number}\n{max_number}")
+    root.destroy()
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+
+def load_range():
+    if os.path.exists('range.txt'):
+        with open('range.txt', 'r') as f:
+            min_number, max_number = f.read().splitlines()
+            min_entry.delete(0, tk.END)
+            min_entry.insert(0, min_number)
+            max_entry.delete(0, tk.END)
+            max_entry.insert(0, max_number)
+
+
+def open_range_window():
+    root.attributes('-topmost', False)
+    range_window = tk.Toplevel(root)
+    range_window.title("修改学号范围")
+    range_window.grab_set()
+
+    label = tk.Label(range_window, text="最小学号: ")
+    label.pack()
+
+    global min_entry
+    min_entry = tk.Entry(range_window)
+    min_entry.pack()
+
+    label = tk.Label(range_window, text="最大学号: ")
+    label.pack()
+
+    global max_entry
+    max_entry = tk.Entry(range_window)
+    max_entry.pack()
+
+    button = tk.Button(range_window, text="保存范围", command=save_range)
+    button.pack()
+
+
+root = tk.Tk()
+root.title("抽取学号")
+root.attributes('-topmost', True)
+
+label = tk.Label(root, text="被抽中的学号是: ")
+label.pack()
+
+button = tk.Button(root, text="抽取学号", command=update_label)
+button.pack()
+
+button = tk.Button(root, text="修改学号范围", command=open_range_window)
+button.pack()
+
+min_entry = tk.Entry(root)
+max_entry = tk.Entry(root)
+
+load_range()
+
+root.mainloop()
--- main_random.py
+++
@@ -1,8 +0,0 @@
-import random
-
-
-def run(number_min, number_max):
-
-    return random.randint(number_max, number_min)
-
-
